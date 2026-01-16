0e0b8bd75704f76ebaba5d34c2d6655402a4ad01
Никита <Goldbergnik1993@gmail.com>
2026-01-16 21:57:57 +0100

    dev

--- todolist/settings.py
+++ todolist/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = "@e2(yx)v&tgh3_s=0yja-i!dpebxsz^dg47x)-k&kq_3zf*9e*"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
@@ -66,8 +66,8 @@ DATABASES = {
         'NAME': 'app_db',
         'USER': 'app_user',
         'PASSWORD': '1234',
-        'HOST': '172.17.0.2',  # You can use a different host if your MySQL server is on a remote machine.
-        'PORT': '',  # Leave this empty to use the default MySQL port (3306).
+        'HOST': 'bd',  # You can use a different host if your MySQL server is on a remote machine.
+        'PORT': '3306',  # Leave this empty to use the default MySQL port (3306).
     }
 }
 
